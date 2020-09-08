0074dccb5761d0c6418b2152e46bcb269e627636
salva-olacua <salvaolacua@gmail.com>
2020-09-08 20:24:06 -0300

    start login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+import './Login.css';
 import Title from './components/Title/Title';
+import Label from './components/Label/Label'
+import Input from './components/Input/Input';
 
 const Login = () => {
+
+    const [ user, setUser ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    function handleChange(name, value) {
+        if(name === 'usuario') {
+            setUser(value)
+        } else {
+            setPassword(value)
+        }
+    };
+
     return (
-        <div>
-            <Title />
+        <div className='login-container'>
+            <Title text='Soy un titulo' />
+            <Label text='Usuario'/>
+            <Input 
+            attribute={{
+                id: 'usuario',
+                name: 'usuario',
+                type: 'text',
+                placeholder: 'Ingrese su ususario'
+            }}
+            handleChange={ handleChange }
+            />
+            <Label text='Contraseña' />
+            <Input 
+            attribute={{
+                id: 'contraseña',
+                name: 'contraseña',
+                type: 'password',
+                placeholder:'Ingrese su contraseña'
+            }}
+            handleChange={ handleChange }
+            />
+            <button>
+                Ingresar
+            </button> 
         </div>
     )
 };
